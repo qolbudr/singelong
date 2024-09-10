a1917311119811cbba0a089c63aa52d388ed197e
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2024-09-10 10:33:20 +0700

    develop: create lyric exception

---
+++ src/types/lyric.ts
@@ -0,0 +1,6 @@
+import { Exception } from "./exception";
+
+export type Lyric = {
+    exception?: Exception;
+    lyric?: string;
+}
--- src/utils/lyric.ts
+++ src/utils/lyric.ts
@@ -1,10 +1,11 @@
 import axios from "axios";
 import { Playing } from "../types/playing";
+import { Lyric } from "../types/lyric";
 
 const token = '2005218b74f939209bda92cb633c7380612e14cb7fe92dcd6a780f';
 const url = 'https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get?format=json&namespace=lyrics_synched&subtitle_format=lrc&app_id=web-desktop-app-v1.0'
 
-export const getLyric = async (playing: Playing): Promise<string> => {
+export const getLyric = async (playing: Playing): Promise<Lyric> => {
     try {
         const param = `&q_artist=${playing.artistName}&q_track=${playing.songTitle}&usertoken=${token}`
         const finalUrl = url + param;
@@ -15,8 +16,8 @@ export const getLyric = async (playing: Playing): Promise<string> => {
             }
         });
         const data = response.data;
-        return data.message.body.macro_calls['track.subtitles.get'].message.body.subtitle_list[0].subtitle.subtitle_body;
+        return { lyric: data.message.body.macro_calls['track.subtitles.get'].message.body.subtitle_list[0].subtitle.subtitle_body }
     } catch (e) {
-        return '';
+        return { exception: { code: 404, message: 'Cannot get lyric for playing song' } };
     }
 }
