8874b2ba49151655759cebf4d029c25000c19c4c
Qolbu Dzikru <qolbu.dzikru@protonmail.com>
2024-09-10 09:53:16 +0700

    develop: change lyrics provider to musixmatch

--- src/utils/lyric.ts
+++ src/utils/lyric.ts
@@ -1,8 +1,22 @@
 import axios from "axios";
 import { Playing } from "../types/playing";
 
-export const getLyric = async (data: Playing): Promise<string> => {
-    const responseLyrics = await axios.get(`https://lyrics-api.qolbudr.workers.dev/?artist=${data.artistName}&name=${data.songTitle}`)
-    const dataLyrics = responseLyrics.data;
-    return dataLyrics;
+const token = '2005218b74f939209bda92cb633c7380612e14cb7fe92dcd6a780f';
+const url = 'https://apic-desktop.musixmatch.com/ws/1.1/macro.subtitles.get?format=json&namespace=lyrics_synched&subtitle_format=lrc&app_id=web-desktop-app-v1.0'
+
+export const getLyric = async (playing: Playing): Promise<string> => {
+    try {
+        const param = `&q_artist=${playing.artistName}&q_track=${playing.songTitle}&usertoken=${token}`
+        const finalUrl = url + param;
+        const response = await axios.get(finalUrl, {
+            headers: {
+                authority: "apic-desktop.musixmatch.com",
+                cookie: "x-mxm-token-guid=",
+            }
+        });
+        const data = response.data;
+        return data.message.body.macro_calls['track.subtitles.get'].message.body.subtitle_list[0].subtitle.subtitle_body;
+    } catch (e) {
+        return '';
+    }
 }
